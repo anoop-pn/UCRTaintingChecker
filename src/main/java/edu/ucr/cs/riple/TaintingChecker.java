bbec4da9c1119b71f3ea0f98504dd3cbee242050
anoop-pn <pn.anoop5@gmail.com>
2022-11-16 00:30:56 -0800

    added checker qual

---
+++ src/main/java/edu/ucr/cs/riple/TaintingChecker.java
@@ -0,0 +1,8 @@
+// package edu.ucr.cs.riple.taint.ucrtainting;
+
+// import org.checkerframework.common.basetype.BaseTypeChecker;
+
+// /**
+//  * This is the entry point for pluggable type-checking.
+//  */
+// public class UCRTaintingChecker extends BaseTypeChecker {}
